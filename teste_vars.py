49c4a445585fbb3d7672d7090795f6b2308a38de
Kaio Cesar <kaioaresi@gmail.com>
2018-12-26 15:27:49 -0200

    Update teste_vars.py

--- teste_vars.py
+++ teste_vars.py
@@ -3,7 +3,7 @@ from os import getenv
 wso2 = getenv('WSO2_APIMANAGER_URL')
 get_token = getenv('AUTHENTICATION_CONFIG_LOGIN_URL')
 
-if wso2 != None and get_token != None and tempo != None:
+if wso2 != None and get_token != None:
     while True:
         print(f'A url do wso2 é : {wso2}\nO token está sendo gerado. pela url {get_token}')
         sleep(30)
