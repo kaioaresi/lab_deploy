d1799183784c8c01d181cf23da2836a6ebd9b9dc
Kaio Cesar <kaioaresi@gmail.com>
2018-12-26 14:29:07 -0200

    Criando "teste_vars.py"

---
+++ teste_vars.py
@@ -0,0 +1,9 @@
+from os import getenv
+
+wso2 = getenv('WSO2_APIMANAGER_URL')
+get_token = getenv('AUTHENTICATION_CONFIG_LOGIN_URL')
+
+if wso2 != None and get_token != None:
+    print(f'A url do wso2 é : {wso2}\nO token está sendo gerado pela url {get_token}')
+else:
+    print('Os valores não informados!')
