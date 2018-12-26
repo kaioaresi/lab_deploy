c6ad9942f9a03e06e139c67d35b449b374af3d37
Kaio Cesar <kaioaresi@gmail.com>
2018-12-26 14:40:29 -0200

    Update teste_vars.py

--- teste_vars.py
+++ teste_vars.py
@@ -6,4 +6,4 @@ get_token = getenv('AUTHENTICATION_CONFIG_LOGIN_URL')
 if wso2 != None and get_token != None:
     print(f'A url do wso2 é : {wso2}\nO token está sendo gerado pela url {get_token}')
 else:
-    print('Os valores não informados!')
+    print('Não foi passado valores para as variaveis (WSO2_APIMANAGER_URL e AUTHENTICATION_CONFIG_LOGIN_URL)!')
