034eafe6fc7d9949248db8ea07ed7233af79360e
Kaio Cesar <kaioaresi@gmail.com>
2018-12-28 16:46:35 -0200

    Update teste_vars.py

--- teste_vars.py
+++ teste_vars.py
@@ -1,2 +1,6 @@
 from os import getenv
-print(f'A url do wso2 é : {getenv("WSO2_APIMANAGER_URL")}\nO token está sendo gerado. pela url {getenv("AUTHENTICATION_CONFIG_LOGIN_URL")}\nvalor generico informado .env "{getenv("TESTE")}"')
+from time import sleep
+
+while True:
+    print(f'A url do wso2 é : {getenv("WSO2_APIMANAGER_URL")}\nO token está sendo gerado. pela url {getenv("AUTHENTICATION_CONFIG_LOGIN_URL")}\nvalor generico informado .env "{getenv("TESTE")}"')
+    sleep(120)
