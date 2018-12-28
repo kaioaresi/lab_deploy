a0c84f2c08afb003aeeda13baba7ca31a25cf4c8
Kaio Cesar <kaioaresi@gmail.com>
2018-12-28 16:27:11 -0200

    Update teste_vars.py

--- teste_vars.py
+++ teste_vars.py
@@ -1,13 +1,4 @@
 from os import getenv
 from time import sleep
 
-wso2 = getenv('WSO2_APIMANAGER_URL')
-get_token = getenv('AUTHENTICATION_CONFIG_LOGIN_URL')
-teste = getenv('TESTE')
-
-if wso2 != None and get_token != None and teste != None:
-    while True:
-        print(f'A url do wso2 é : {wso2}\nO token está sendo gerado. pela url {get_token}, valor generico informado .env "{teste}"')
-        sleep(30)
-else:
-    print('Os valores não informados!')
+print(f'A url do wso2 é : {getenv("WSO2_APIMANAGER_URL")}\nO token está sendo gerado. pela url {getenv("AUTHENTICATION_CONFIG_LOGIN_URL")}\nvalor generico informado .env "{getenv("TESTE")}"')
